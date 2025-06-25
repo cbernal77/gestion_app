6f0f931266780b25e33ec5350188c2475ab0f7ca
Carlos Bernal <fbac77@yahoo.com>
2025-06-25 16:55:37 -0400

    "Comenta console.log de la variable de entorno"

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const productoRoutes = require('./routes/producto.routes');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
+//console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 
 
